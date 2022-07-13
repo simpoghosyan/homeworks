892a498fdae8ba23c67162787136127e53953892
Simon <poghosyan2014@bk.ru>
2022-07-14 00:14:21 +0400

    polechudes

--- polechudes.py
+++ polechudes.py
@@ -5,9 +5,6 @@ count_life = False
 count_limit = 3
 for i in text:
     value = i.split(":")
-for i in value[1]:
-    print(ord(i))
-print(value[1],"-----")
 answer = value[1].strip()
 stars = "*" * len(answer)
 print(stars)
